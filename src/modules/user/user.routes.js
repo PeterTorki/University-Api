5e101952b161acc21cb5042317f6a69074dc1abc
Peter Torki <pjturki@gmail.com>
2025-10-19 00:06:19 +0300

    updates to match angular

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -1,19 +1,15 @@
 import express from "express";
-import {
-  deleteUser,
-  getSpecificUser,
-  getUser,
-  updateUser,
-} from "./user.controller.js";
+import { deleteUser, getSpecificUser, getUser, updateUser } from "./user.controller.js";
 import { auth } from "../../middleware/auth.middleware.js";
 import { checkRole } from "../../middleware/checkRole.middleware.js";
 import { hashPassword } from "../../middleware/hashPass.middleware.js";
 import { validate } from "../../middleware/validations.middleware.js";
 
 const userRouter = express.Router();
-userRouter.get("/", auth(), checkRole(), getUser);
+// userRouter.get("/", auth(), checkRole(), getUser);
+userRouter.get("/", getUser);
 userRouter.get("/:id", auth(), checkRole(), getSpecificUser);
 userRouter.delete("/:id", auth(), checkRole(), deleteUser);
-userRouter.put("/:id", auth(), hashPassword(),validate("updateUser"), updateUser);
+userRouter.put("/:id", auth(), hashPassword(), validate("updateUser"), updateUser);
 
 export default userRouter;
